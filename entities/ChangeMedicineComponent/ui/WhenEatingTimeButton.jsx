e3acae6e08d685c8c890a4e2b91f27c820593c4b
yeeun <yeeun@yeeunui-MacBookAir.local>
2024-08-17 15:30:12 +0900

    약 복용 완료 페이지 일부 제작

---
+++ entities/ChangeMedicineComponent/ui/WhenEatingTimeButton.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+
+export default function WhenEatingTimeButton() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonPress = (button) => {
+    setSelectedButton(button);
+  };
+
+  return (
+    <Container>
+      <ButtonContainer>
+        <StyledButton
+          onPress={() => handleButtonPress('식전')}
+          isSelected={selectedButton === '식전'}
+        >
+          <ButtonText isSelected={selectedButton === '식전'}>식전</ButtonText>
+        </StyledButton>
+        <StyledButton
+          onPress={() => handleButtonPress('식후')}
+          isSelected={selectedButton === '식후'}
+        >
+          <ButtonText isSelected={selectedButton === '식후'}>식후</ButtonText>
+        </StyledButton>
+      </ButtonContainer>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 327px;
+  height: 110px;
+  margin-top: -100px;
+`;
+
+const StyledButton = styled.TouchableOpacity`
+  display: flex;
+  width: 157px;
+  height: 110px;
+  padding: 41px 61px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  border-radius: 24px;
+  background: ${(props) => (props.isSelected ? '#E5E9FA' : '#E5E5EC')}; /* 선택된 경우 배경색 변경 */
+    border:${(props) => (props.isSelected ? '#91A6F3' : '#E5E5EC')}; /* 선택된 경우 테두리 변경 */
+
+`;
+
+const ButtonText = styled.Text`
+    color: ${(props) => (props.isSelected ? '#3C63EC' : '#767676')}; /* 선택된 경우 텍스트 색상 변경 */
+    font-family: Pretendard;
+    text-align: center;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 28px; /* 140% */
+    background: ${(props) => (props.isSelected ? '#E5E9FA' : '#E5E5EC')}; /* 선택된 경우 배경색 변경 */
+`;
--- pages/ChangeMedicine.jsx
+++ pages/ChangeMedicine.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import {View, Text} from 'react-native' 
-// import ToolBar_x from '../entities/MedicineTimeSettingComponent/ui/Toolbar_x';
-import ToolBar from '../entities/MedicineTimeSettingComponent/ui/Toolbar';
-import MedicineText from '../entities/MedicineTimeSettingComponent/ui/MedicineText';
+import ToolBar_x from '../entities/MedicineTimeSettingComponent/ui/Toolbar_x';
+import WhenMedicineText from '../entities/ChangeMedicineComponent/ui/WhenMedicineText';
 import { useNavigation } from '@react-navigation/native';
 import EatingTimeButton from '../entities/MedicineTimeSettingComponent/ui/EatingTimeButton';
 import MealButton from '../entities/MedicineTimeSettingComponent/ui/MealButton';
-import ChangeTimeButton from '../entities/MedicineTimeSettingComponent/ui/ChangeTimeButton';
+import ChangeTimeButton from '../entities/ChangeMedicineComponent/ui/ChangeTimeButton';
 import ChangeTimeButtonChangeTimeButton from '../entities/MedicineTimeSettingComponent/ui/ChangeTimeButton';
 import StandardButton from '../shared/component/StandardButton';
-export default function MedicineTimeSetting() {
-  
+import WhenEatingTimeButton from '../entities/ChangeMedicineComponent/ui/WhenEatingTimeButton';
+import Main from '../pages/Main';
+export default function ChangeMedicine() {
+ 
   const [isComplete, setIsComplete] = useState(false);
   const navigation = useNavigation();
   
   const handleButtonClick = () => {
     if (isComplete) {
-      navigation.navigate("ChangeMedicine");
+      navigation.navigate("MedicineComplete");
     } else {
       setIsComplete(true);
     }
@@ -25,11 +26,11 @@ export default function MedicineTimeSetting() {
   return(
     <MainLayout>
           {/* 툴바 */}
-            {!isComplete && <ToolBar/>}
+            {!isComplete && <ToolBar_x/>}
           {/* 텍스트 */}
-          {!isComplete && <MedicineText/>}
+          {!isComplete && <WhenMedicineText/>}
           {/* 식전 식후 버튼  */}
-          {!isComplete && <EatingTimeButton/>}
+          {!isComplete && <WhenEatingTimeButton/>}
           {/* 아침, 점심, 저녁 버튼 */}
           {!isComplete && <MealButton/>}
           {/* 재설정하기 버튼 */}
