08af4249e2a739211089aff458eb3d05ecf3e691
Juye0nLee <dlwndus708@naver.com>
2024-08-22 15:55:28 +0900

    ✨ 오늘의탭 circle progress bar 추가

--- App.js
+++ App.js
@@ -1,53 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native';
-import Main from './pages/Main';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import FirstSplash from './pages/FirstSplash';
+import Start from './pages/Start';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Main from './pages/Main';
 import HospitalTimeSetting from './pages/HospitalTimeSetting';
 import MedicineTimeSetting from './pages/MedicineTimeSetting';
 import ChangeMedicine from './pages/ChangeMedicine';
 import MedicineComplete from './pages/MedicineComplete';
 import TodaysShim from './pages/TodaysShim';
 import NavigationBar from './shared/component/NavigationBar';
+
 const Stack = createStackNavigator();
 
-export default function App() {
+function MainScreenWithNavBar() {
+  return (
+    <>
+      <Main />
+      <NavigationBar />
+    </>
+  );
+}
+
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Main"
-        >
-        <Stack.Screen 
-          name="Main" 
-          component={Main}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
-        <Stack.Screen 
-          name="MedicineTimeSetting" 
-          component={MedicineTimeSetting}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
-        <Stack.Screen 
-          name="HospitalTimeSetting" 
-          component={HospitalTimeSetting}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
-        <Stack.Screen 
-          name="ChangeMedicine" 
-          component={ChangeMedicine}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
-          <Stack.Screen 
-          name="MedicineComplete" 
-          component={MedicineComplete}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
-          <Stack.Screen 
-          name="TodaysShim" 
-          component={TodaysShim}
-          options={{ headerShown: false }} //탭바 안보이게
-        />
+      <Stack.Navigator
+        initialRouteName="FirstSplash"
+        screenOptions={{ headerShown: false }} //모든 스크린에서 헤더를 숨김
+      >
+        <Stack.Screen name="FirstSplash" component={FirstSplash}/>
+        <Stack.Screen name="Start" component={Start}/>
+        <Stack.Screen name="SignUp" component={SignUp}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Main" component={MainScreenWithNavBar} />
+        <Stack.Screen name="MedicineTimeSetting" component={MedicineTimeSetting} />
+        <Stack.Screen name="HospitalTimeSetting" component={HospitalTimeSetting} />
+        <Stack.Screen name="ChangeMedicine" component={ChangeMedicine} />
+        <Stack.Screen name="MedicineComplete" component={MedicineComplete} />
+        <Stack.Screen name="TodaysShim" component={TodaysShim} />
       </Stack.Navigator>
-      <NavigationBar/>
     </NavigationContainer>
   );
 }
+
+export default App;
