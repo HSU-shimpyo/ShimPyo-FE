55fc37ae275a6d04b90b2274cee17e4a2864e3e4
yeeun <yeeun@yeeunui-MacBookAir.local>
2024-08-23 23:01:14 +0900

    약 복용 알림 시간 페이지 수정

--- entities/MedicineTimeSettingComponent/ui/EatingTimeButton.jsx
+++ entities/MedicineTimeSettingComponent/ui/EatingTimeButton.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 
 export default function EatingTimeButton() {
-  const [selectedButton, setSelectedButton] = useState(null);
-
-  const handleButtonPress = (button) => {
-    setSelectedButton(button);
+  // 더미 데이터 (하나만 선택되도록 설정)
+  const selectedData = {
+    option: '식후', // 선택된 옵션
+    time: null,     // 이 경우 선택되지 않음
   };
 
-  //내가 현재 설정한 페이지이므로, 데이터가 들어가야 하는 페이지
+  // 현재 설정된 페이지로, 더미 데이터를 기반으로 색상이 적용된 버튼을 표시
   return (
     <Container>
       <ButtonContainer>
         <StyledLeftButton
-          onPress={() => handleButtonPress('식후')}
-          isSelected={selectedButton === '식후'}
+          isSelected={selectedData.option === '식후' && !selectedData.time}
         >
-          <ButtonText isSelected={selectedButton === '식후'}>식후</ButtonText>
+          <ButtonText isSelected={selectedData.option === '식후' && !selectedData.time}>
+            식후
+          </ButtonText>
         </StyledLeftButton>
         <StyledRightButton
-          onPress={() => handleButtonPress('30분')}
-          isSelected={selectedButton === '30분'}
+          isSelected={!selectedData.option && selectedData.time === '30분'}
         >
-          <ButtonText isSelected={selectedButton === '30분'}>30분</ButtonText>
+          <ButtonText isSelected={!selectedData.option && selectedData.time === '30분'}>
+            30분
+          </ButtonText>
         </StyledRightButton>
       </ButtonContainer>
     </Container>
@@ -40,36 +42,35 @@ const ButtonContainer = styled.View`
   justify-content: space-between;
   width: 327px;
   height: 64px;
-  margin-top:-100px;
+  margin-top: -100px;
 `;
 
-const StyledLeftButton = styled.TouchableOpacity`
+const StyledLeftButton = styled.View`
   width: 164px;
   height: 64px;
-  background-color: ${(props) => (props.isSelected ? '#C2DDF8' : '#FFFFFF')}; /* 선택된 경우 배경색 변경 */
+  background-color: ${(props) => (props.isSelected ? '#C2DDF8' : '#FFFFFF')};
   justify-content: center;
   align-items: center;
   border-radius: 24px 0px 0px 24px;
 `;
 
-const StyledRightButton = styled.TouchableOpacity`
+const StyledRightButton = styled.View`
   width: 164px;
   height: 64px;
-  background-color: ${(props) => (props.isSelected ? '#C2DDF8' : '#FFFFFF')}; /* 선택된 경우 배경색 변경 */
+  background-color: ${(props) => (props.isSelected ? '#C2DDF8' : '#FFFFFF')};
   justify-content: center;
   align-items: center;
-border-radius: 0px 24px 24px 0px;
+  border-radius: 0px 24px 24px 0px;
 `;
 
 const ButtonText = styled.Text`
-  color: ${(props) => (props.isSelected ? '#111111' : '#111111')}; /* 선택된 경우 텍스트 색상 변경 */
-text-align: center;
-
-/* MO/Title/KR/T4_KR_Sb */
-font-family: Pretendard;
-font-size: 16px;
-font-style: normal;
-font-weight: 600;
-line-height: 24px; /* 150% */
-letter-spacing: -0.4px;
+  color: #111111;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px; /* 150% */
+  letter-spacing: -0.4px;
 `;
+
--- entities/MedicineTimeSettingComponent/ui/MealButton.jsx
+++ entities/MedicineTimeSettingComponent/ui/MealButton.jsx
@@ -1,42 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
 
 export default function MealButton() {
-  const [selectedButton, setSelectedButton] = useState(null);
-
-  const handleButtonPress = (button) => {
-    setSelectedButton(button);
+  // 더미 데이터
+  const mealTimes = {
+    아침: '09:30',
+    점심: '12:30',
+    저녁: '18:00'
   };
 
-  //내가 현재 설정한 페이지이므로, 데이터가 들어가야 하는 페이지
   return (
     <Container>
       <ButtonContainer>
-        <StyledButton
-          onPress={() => handleButtonPress('아침')}
-          isSelected={selectedButton === '아침'}
-        >
-          <ButtonText isSelected={selectedButton === '아침'}>아침</ButtonText>
-          <ButtonText isSelected={selectedButton === '아침'}>09:00</ButtonText>
-
+        <StyledButton>
+          <ButtonText>아침</ButtonText>
+          <ButtonText>{mealTimes['아침']}</ButtonText>
         </StyledButton>
-        <StyledButton
-          onPress={() => handleButtonPress('점심')}
-          isSelected={selectedButton === '점심'}
-        >
-          <ButtonText isSelected={selectedButton === '점심'}>점심</ButtonText>
-          <ButtonText isSelected={selectedButton === '점심'}>12:30</ButtonText>
-
+        <StyledButton>
+          <ButtonText>점심</ButtonText>
+          <ButtonText>{mealTimes['점심']}</ButtonText>
         </StyledButton>
-        <StyledButton
-          onPress={() => handleButtonPress('저녁')}
-          isSelected={selectedButton === '저녁'}
-        >
-          <ButtonText isSelected={selectedButton === '저녁'}>저녁</ButtonText>
-          <ButtonText isSelected={selectedButton === '저녁'}>18:00</ButtonText>
-
+        <StyledButton>
+          <ButtonText>저녁</ButtonText>
+          <ButtonText>{mealTimes['저녁']}</ButtonText>
         </StyledButton>
-        
       </ButtonContainer>
     </Container>
   );
@@ -53,35 +40,27 @@ const ButtonContainer = styled.View`
   justify-content: space-between;
   width: 327px;
   height: 64px;
-  margin-top:-340px;
-    color: ${(props) => (props.isSelected ? '#111111' : '#111111')}; /* 선택된 경우 텍스트 색상 변경 */
-
+  margin-top: -340px;
 `;
 
-
-
-const StyledButton = styled.TouchableOpacity`
+const StyledButton = styled.View`
   width: 100px;
-    height: 100px;
-    flex-shrink: 0;
-  background-color: ${(props) => (props.isSelected ? '#FFFFFF' : '#FFFFFF')}; /* 선택된 경우 배경색 변경 */
-justify-content: center;
+  height: 100px;
+  flex-shrink: 0;
+  background-color: #FFFFFF; /* 배경색 고정 */
+  justify-content: center;
   align-items: center;
   border-radius: 24px;
-opacity: var(--sds-size-stroke-border);
-background: #FFF;
 `;
 
 const ButtonText = styled.Text`
-  color: ${(props) => (props.isSelected ? '#111111' : '#111111')}; /* 선택된 경우 텍스트 색상 변경 */
-color: var(--Font-02_black, #111);
-text-align: center;
-
-/* MO/Title/KR/T4_KR_Sb */
-font-family: Pretendard;
-font-size: 16px;
-font-style: normal;
-font-weight: 600;
-line-height: 24px; /* 150% */
-letter-spacing: -0.4px;
+  color: #111111;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px; /* 150% */
+  letter-spacing: -0.4px;
 `;
+
