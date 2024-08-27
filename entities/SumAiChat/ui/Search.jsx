15ff447296b0477d535eeabfd6d9268c98e9a9ac
Juye0nLee <dlwndus708@naver.com>
2024-08-27 13:59:37 +0900

    ✨ 질문방 목록 검색창 focus 되면 배경색  바뀌도록

--- entities/SumAiChat/ui/Search.jsx
+++ entities/SumAiChat/ui/Search.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import glasses from '../../../assets/images/glasses2.png'
 export default function Search() {
+  const [isFocus, setIsFocus] = useState(false);
   return (
-    <WrapInput>
+    <WrapInput
+      backgroundColor={isFocus ? "#FFFFFF" : "#F1F1F5"}
+      border={isFocus ? "1px solid #3C63EC" : "1px solid #E5E5EC"}
+    >
         <Icon source={glasses}/>
         <Input
-            placeholder="검색어를 입력하세요"
+            placeholder={isFocus ? "" : "검색어를 입력하세요"}
             placeholderTextColor="#767676"
             returnKeyType="done"
+            onFocus={()=>setIsFocus(true)}
+            onSubmitEditing={()=>setIsFocus(false)}
         />
     </WrapInput>
   )
@@ -16,8 +22,9 @@ export default function Search() {
 const WrapInput = styled.View`
 width : 327px;
 height : 56px;
-background-color : #F1F1F5; 
-border : 1px solid #E5E5EC;
+background-color : ${({ backgroundColor }) => backgroundColor || '#F1F1F5'};
+line-height: 28px;; 
+border : ${({ border }) => border || '1px solid #E5E5EC'};
 border-radius : 100px;
 margin-top : 8px;
 margin-bottom : 150px;
